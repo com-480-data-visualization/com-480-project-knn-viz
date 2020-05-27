16a0a2f5378209184944a3497cc2f7b8f0e06f6b
Natalie Bolón <natalie.bolonbrun@epfl.ch>
2020-05-27 17:30:38 +0200

    add link to video and repo

--- try_plots/title.js
+++ try_plots/title.js
@@ -1,5 +1,5 @@
 var width_title = Math.min(1800, window.innerWidth) - 270;//width_timeline = 200 + width_legend_timeline=100;
-console.log(width_title)
+
 var height_title = 80;
 
 var title_games = d3.select("#title")
@@ -7,16 +7,55 @@ var title_games = d3.select("#title")
                 .attr("width", width_title)
                 .attr("height", height_title)
                 .attr("transform","translate(" + 0 +",0)");
+var openNewTab = function openInNewTab(url) {
+                        var win = window.open(url, '_blank');
+                        win.focus();
+                        }
+// add icon github repository
+      title_games
+        .append("svg:image")
+        .attr('x', 20)
+        .attr('y', 20)
+        .attr('width', 20)
+        .attr('height', 20)
+        .attr("xlink:href", "images/github.jpeg")
+        .on("click", function(){openNewTab("https://github.com/com-480-data-visualization/com-480-project-knn-viz")});
+
+// add icon process book
+      title_games
+        .append("svg:image")
+        .attr('x', 60)
+        .attr('y', 20)
+        .attr('width', 20)
+        .attr('height', 20)
+        .attr("xlink:href", "images/process.jpeg")
+        .on("click", function(){openNewTab("https://github.com/com-480-data-visualization/com-480-project-knn-viz")});
+
+// add icon video
+      title_games
+            .append("svg:image")
+            .attr('x', 100)
+            .attr('y', 20)
+            .attr('width', 20)
+            .attr('height', 20)
+            .attr("xlink:href", "images/video.png")
+            .on("click", function(){openNewTab("https://youtu.be/xWv1JyJC_aY")});
+
+
 
 function update_title(year, city, country){
   title_games.selectAll("text").remove();
-
-    title_games.append("text")
+    // add text
+      title_games.append("text")
             .attr("x", (width_title)/2)
             .attr("y", 50)
             .text("Discovering the Olympic games of " + year + " (" + city + ", " + country + ")")
             .attr("font-family", "Oswald")
-            .attr("font-size", "25px")
+            .attr("font-size", function(){if(width_title < 700){
+                    return "20px";
+            } else {
+                    return "25px";
+            }})
             .attr("font-weight", 900)
             .attr("alignment-baseline","middle")
             .attr("text-anchor", "middle");
